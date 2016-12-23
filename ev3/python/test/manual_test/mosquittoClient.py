37a2fdd5f4b6785bafcfaad8341b334a5a9a5263
rcristino <ricardocristino@gmail.com>
2016-12-23 21:26:03 +0100

    Add manual test for mosquitto

---
+++ ev3/python/test/manual_test/mosquittoClient.py
@@ -0,0 +1,12 @@
+
+
+#!/usr/bin/env python
+
+import paho.mqtt.client as mqtt
+
+# This is the Publisher
+
+client = mqtt.Client()
+client.connect("localhost",1883,60)
+client.publish("topic/test", "Hello world!");
+client.disconnect();
