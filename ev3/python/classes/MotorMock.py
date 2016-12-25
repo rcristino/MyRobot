b5e4d947a293dbf3da930b66014ea71c3a6159b0
rcristino <ricardocristino@gmail.com>
2016-12-25 20:53:41 +0100

    Add MotocMock for tests

---
+++ ev3/python/classes/MotorMock.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+class Motor:
+    def __init__(self, name):
+        self.speed = 0
+        self.name = name
+        self.motor = "Motor" + name
+        self.direction = 1
+        self.stop()
+        print("MotorMock")
+
+    def getName(self):
+        return self.name
+
+    def getSpeed(self):
+        return self.speed
+
+    def setSpeed(self, speed):
+        self.speed = speed
+        print("setSeed: " + str(speed))
+
+    def getDirection(self):
+        return self.direction
+
+    def setDirection(self, direction):
+        self.direction = direction
+        print("setDirection")
+
+    def movePosition(self, position, speed, action="hold"):
+        self.speed = speed * self.direction
+        print("movePosition: " + str(speed))
+
+    def moveTime(self, speed, intime):
+        self.speed = speed * self.direction
+        print("moveTime: " + str(speed))
+
+    def move(self, speed):
+        self.speed = speed * self.direction
+        print("move: " + str(speed))
+
+    def stopRelax(self):
+        print("stopRelax")
+
+    def stop(self):
+        print("stop")
+
+    def wait(self):
+        print("wait")
--- ev3/python/main.py
+++ ev3/python/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-from classes.Motor import Motor
+from classes.MotorMock import Motor
+#from classes.MotorMock import Motor
 from time import sleep
 
 def main():
